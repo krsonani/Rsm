10be2308e86c5ee3358db7a350fe672f2dfd64eb
Krunalbhai Sonani <K.Sonani@maxxtonind.local>
2023-05-22 14:48:23 +0530

    first commit

---
+++ src/main/java/com/sm/rsm/RsmApplication.java
@@ -0,0 +1,13 @@
+package com.sm.rsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/rsm/RsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.rsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
